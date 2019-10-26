dd929dd30bcd29ff4eb9d3c36f4e307bf04ad470
lxde in asus trio <linux-trio@serprest.pt>
2019-10-27 00:45:43 +0100

    some bugs

--- mv2original.sh
+++ mv2original.sh
@@ -10,10 +10,11 @@ SRC="${1:-.}"
  
 while read selected
 do
-  dir="${selected%/*/*.jpg}"
+  dir="${selected%/*/*.???}"          #use ??? instead of jpg because it could be JPG
   original="$dir/original"
   [[ -d $original ]] || mkdir -pv "$original"
   filename="${selected##*/}" 
-  file="${filename%.jpg}"
-  find "$dir" -maxdepth 1 -name "*$file.*" -exec echo mv -vf "{}" "$original"/ ';'
+  file="${filename%.???}"
+  #find "$dir" -maxdepth 1 -name "*$file.*" -exec mv -vf "{}" "$original"/ ';' #don't use this because the bug "same file"
+  find "$dir" -maxdepth 1 -name "*$file.*" -exec cp -vlf "{}" "$original"/ ';' -exec unlink "{}" ';'
 done < <(find "$SRC" -ipath '*/sel/*.jpg')
