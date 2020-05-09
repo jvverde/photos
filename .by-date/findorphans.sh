49a05099fc4c4d470ca807b2df78ab45859cadd9
lxde in asus trio <linux-trio@serprest.pt>
2020-05-09 01:43:27 +0100

    add by-date-*

--- .by-date/findorphans.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-SDIR=$(readlink -e "$(dirname $0)")
-
-find . -mindepth 2 -type f -links 1
---
+++ by-date-taken.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+declare -r sdir=$(readlink -e "$(dirname $0)")
+
+[[ $1 =~ - || -z $1 || -z $2 ]] && {
+	echo -e "Usage:\n\t$0 src1 [src2[...[srcN]]] dst"
+	exit 1
+}
+
+declare -a src=("${@:1:$#-1}")
+declare -r dst="${@: -1}"
+
+while IFS= read file
+do
+	while IFS=$'\t' read -r DT MD
+	do
+		[[ $DT == - ]] && continue
+		declare name="$(basename -- "$file")"
+		declare parent="$(dirname -- "$file")"
+		from="$parent/$name"
+		ymd=${DT% *}
+		hms=${DT#* }
+		h=${hms%%-*}
+		prefix="$hms"
+		to="$dst/$ymd/$h/$prefix.$name"
+		mkdir -pv "$(dirname -- "$to")"
+		[[ $from -ef $to ]] || ln -vfT "$from" "$to"
+	done < <(exiftool -d "%Y/%m/%d %H-%M-%S" -T -DateTimeOriginal -FileModifyDate -sort "$file")
+done < <(find "${src[@]}" -type f -print)
