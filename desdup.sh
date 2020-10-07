99066877d5da9629e75be73732cdb9f5359e722e
Pica <Pica@bkit.pt>
2020-10-07 12:45:29 +0100

    Don't break previous hardlinks

--- desdup.sh
+++ desdup.sh
@@ -4,6 +4,7 @@
 sdir="$(readlink -e "$(dirname $0)")"
 
 dir="${1:-.}"
+shift
 
 declare -i oldsize=0
 declare -i oldinum=0
@@ -12,12 +13,12 @@ declare oldfile=""
 while read inum hlinks size file
 do
   (( oldinum == inum )) && continue # file and oldfile are already hardlink of each other
-  (( oldsize == size )) && cmp -s "$oldfile" "$file" && ln -vfT "$oldfile" "$file"
+  (( oldsize == size )) && cmp -s "$oldfile" "$file" && ln -vfT "$oldfile" "$file" && continue
 
   oldsize=$size
   oldfile="$file"
   oldinum=$inum
 done < <(
-  find "$dir" -type f -printf "%i %n %s %p\n" |
-  sort -k3nr,3 -k2nr,2 -k1n,1 -k4
+  find "$dir" -type f ${@+$@} -printf "%i %n %s %p\n" |
+  sort -k3n,3 -k2nr,2 -k1n,1 -k4
 )
