a14520a12379acd64add2263bb5e123018bb57e7
Isidro <jvverde@rolieiro>
2022-05-11 19:43:44 +0100

    bugs fixed

--- DxO-not.sh
+++ DxO-not.sh
@@ -5,11 +5,11 @@ sdir="$(readlink -e "$(dirname $0)")"
 
 dir="${1:-.}"
 
-$ find "$dir" -type d -iname 'dxo'|
+find "$dir" -type d -iname 'dxo'|
 	while read dirname;
 	do
-		a=$(find "$dirname" -maxdepth 1 -type f|wc -l);
-		b=$(find "$dir/.." -maxdepth 1 -type f|wc -l);
-		echo "$a|$b|$dir"; 
+		a=$(find "$dirname" -type f ! -ipath '*/sel/*'|wc -l);
+		b=$(find "$dirname/.." -maxdepth 1 -type f|wc -l);
+		echo "$a|$b|$dirname"; 
 	done |
 	awk -F\| '$1 != $2 {print}'
