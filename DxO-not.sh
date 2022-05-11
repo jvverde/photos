93834274fe77884234958a6dd5f5e7bd656e3108
Isidro <jvverde@rolieiro>
2022-05-11 19:51:46 +0100

    improvement

--- DxO-not.sh
+++ DxO-not.sh
@@ -8,8 +8,8 @@ dir="${1:-.}"
 find "$dir" -type d -iname 'dxo'|
 	while read dirname;
 	do
-		a=$(find "$dirname" -type f ! -ipath '*/sel/*'|wc -l);
-		b=$(find "$dirname/.." -maxdepth 1 -type f|wc -l);
+		a=$(find "$dirname" -type f ! -ipath '*/sel/*' -iname '*.jpg'|wc -l);
+		b=$(find "$dirname/.." -maxdepth 1 -type f -iname '*.nef'|wc -l);
 		echo "$a|$b|$dirname"; 
 	done |
 	awk -F\| '$1 != $2 {print}'
